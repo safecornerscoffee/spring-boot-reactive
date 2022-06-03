f88d5526508bdc36cc0e5a783fd64abd4badca95
safecorners <safeconers.mobile@gmail.com>
2022-06-03 17:43:32 +0900

    ch.08 - RSocket Client

---
+++ rsocket-client/src/main/java/io/safecorners/rsocketclient/RsocketClientApplication.java
@@ -0,0 +1,13 @@
+package io.safecorners.rsocketclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsocketClientApplication.class, args);
+	}
+
+}
---
+++ rsocket-client/src/test/java/io/safecorners/rsocketclient/RsocketClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.safecorners.rsocketclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
